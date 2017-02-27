4e766f2b82b50282e13eb9eee35b1995c611e5bf
andybergon <andybergon@gmail.com>
2017-02-27 17:09:28 +0100

    implement getDialForString and add test cases
    Andrea Bergonzo - Coding Challenge

--- src/main/java/org/interview/problem/KeypadUtils.java
+++ src/main/java/org/interview/problem/KeypadUtils.java
@@ -7,8 +7,28 @@ public class KeypadUtils {
      * @return The appropriate dial pad numbers for the given text
      * @throws Exception Throws the appropriate exception for invalid input
      */
-    public static String getDialForString(final String text) {
-        return null;
+    public static String getDialForString(final String text) throws Exception {
+        if (text == null) {
+            throw new IllegalArgumentException(); // can be IllegalArgumentException or NullPointerException
+        }
+
+        StringBuilder sb = new StringBuilder();
+        AlphabetConstants alphabetConstants = new AlphabetConstants();
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            c = Character.toLowerCase(c);
+
+            if (alphabetConstants.KEY_PAD_CHAR.containsKey(c)) {
+                sb.append(alphabetConstants.KEY_PAD_CHAR.get(c));
+            } else if (Character.isDigit(c)) {
+                sb.append(c);
+            } else {
+                sb.append(alphabetConstants.ALL_OTHER_CHARS);
+            }
+        }
+
+        return sb.toString();
     }
 
 }
--- src/test/java/org/interview/problem/test/unit/KeypadUtilsTest.java
+++ src/test/java/org/interview/problem/test/unit/KeypadUtilsTest.java
@@ -17,7 +17,7 @@ public class KeypadUtilsTest {
     }
 
     @Test
-    public void testKeypadSuccess() throws Exception{
+    public void testKeypadSuccess() throws Exception {
         Map<String, String> testValues = new HashMap<>();
         testValues.put("Hello", "43556");
         testValues.put("how are you today ?", "4690273096808632901");
@@ -31,10 +31,41 @@ public class KeypadUtilsTest {
     }
 
     @Test
-    public void testKeypadSuccessWithNumbers() throws Exception{
+    public void testKeypadSuccessWithNumbers() throws Exception {
         Map<String, String> testValues = new HashMap<>();
         testValues.put("Hell4o #*", "435546011");
         testValues.put("how are you today ?", "4690273096808632901");
+        testValues.entrySet().forEach(entry -> {
+            try {
+                assertEquals(entry.getValue(), KeypadUtils.getDialForString(entry.getKey()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void testKeypadSuccessNoAlphabet() throws Exception {
+        Map<String, String> testValues = new HashMap<>();
+        testValues.put("1234", "1234"); // only numbers
+        testValues.put("    ", "0000"); // only whitespace
+        testValues.put("!()+-*/", "1111111"); // only other character
+        testValues.put(" !15 +", "011501"); // mixed
+        testValues.entrySet().forEach(entry -> {
+            try {
+                assertEquals(entry.getValue(), KeypadUtils.getDialForString(entry.getKey()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void testKeypadSuccessMixedCases() throws Exception {
+        Map<String, String> testValues = new HashMap<>();
+        testValues.put("hello ben", "435560236"); // only lowercase
+        testValues.put("HELLO BEN", "435560236"); // only uppercase
+        testValues.put("HeLLo BeN", "435560236"); // mixed cases
         testValues.entrySet().forEach(entry -> {
             try {
                 assertEquals(entry.getValue(), KeypadUtils.getDialForString(entry.getKey()));
@@ -49,6 +80,20 @@ public class KeypadUtilsTest {
         Map<String, String> testValues = new HashMap<>();
         testValues.put("Whats up?", "1234");
         testValues.put("#123{]=-", "1234");
+        testValues.entrySet().forEach(entry -> {
+            try {
+                assertNotEquals(entry.getValue(), KeypadUtils.getDialForString(entry.getKey()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void testKeypadFailureNoAlphabet() throws Exception {
+        Map<String, String> testValues = new HashMap<>();
+        testValues.put("1234", "1111");
+        testValues.put("!12/", "1111");
         testValues.entrySet().forEach(entry -> {
             try {
                 assertNotEquals(entry.getValue(), KeypadUtils.getDialForString(entry.getKey()));
